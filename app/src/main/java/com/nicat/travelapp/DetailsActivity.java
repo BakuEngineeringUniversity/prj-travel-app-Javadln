35a42362c7ab85c721069b123d9447d33acacd72
Javadln <126745463+Javadln@users.noreply.github.com>
2024-01-26 03:38:20 +0400

    button update

--- app/src/main/java/com/nicat/travelapp/DetailsActivity.java
+++ app/src/main/java/com/nicat/travelapp/DetailsActivity.java
@@ -2,7 +2,10 @@ package com.nicat.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ImageView backButton = findViewById(R.id.imageView4);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
+
 }
