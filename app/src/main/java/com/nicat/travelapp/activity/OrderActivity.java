de31dd035940b1ffb800e98c2a6c474dd2bc79a8
Javadln <126745463+Javadln@users.noreply.github.com>
2024-01-26 13:50:28 +0400

    P

--- app/src/main/java/com/nicat/travelapp/activity/OrderActivity.java
+++ app/src/main/java/com/nicat/travelapp/activity/OrderActivity.java
@@ -1,4 +1,16 @@
 package com.nicat.travelapp.activity;
 
-public class OrderActivity {
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nicat.travelapp.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
 }
